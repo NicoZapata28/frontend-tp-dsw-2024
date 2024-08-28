74144004c307951cd86415f5b2281fb7ef0bea12
valentttino <vgboggio@gmail.com>
2024-08-28 20:32:59 -0300

    add axios and hello world

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+//import axios from 'axios'
 
+/*axios.get('').then(response => {
+  const materials = response.data
+  console.log(materials)
+})
+*/
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
