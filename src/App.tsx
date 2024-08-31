7d64958c0d0b74dc96cec8e16a097ee25285651c
valentttino <vgboggio@gmail.com>
2024-08-31 20:45:32 -0300

    create Materials component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { useState, useEffect } from "react"
-import materialsService from "./services/materials.ts"
-import { IMaterial } from "./services/materials.ts"
+import Materials from "./components/Materials.tsx"
 
 const App = () => {
-  const [materials, setMaterials] = useState<IMaterial[]>([])
-  
-  useEffect(() => {
-    materialsService.getAll().then(data =>
-      setMaterials(data)
-    )
-  }, [])
-
   return (
     <div>
-      <h1>Materials</h1>
-      {(materials.map(m =>
-        <li className="materials" key={m.id}>
-          {m.name} | {m.description} | ${m.cost} | Stock: {m.stock}
-        </li>
-      ))}
+      <Materials />
     </div>
   )}
 
---
+++ src/components/Materials.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+import materialsService from "../services/materials.ts"
+import { IMaterial } from "../services/materials.ts"
+
+const Materials = () =>{
+  const [materials, setMaterials] = useState<IMaterial[]>([])
+  
+  useEffect(() => {
+    materialsService.getAll().then(data =>
+      setMaterials(data)
+    )
+  }, [])
+
+  return (
+    <div>
+      <h1>Materials</h1>
+      {(materials.map(m =>
+        <li className="materials" key={m.id}>
+          {m.name} | {m.description} | ${m.cost} | Stock: {m.stock}
+        </li>
+      ))}
+    </div>
+  )
+}
+
+export default Materials
