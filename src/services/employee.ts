b851656008570e3146c98a8b93ae6e3218df3d6c
martularrauri <martibelenlarrauri@gmail.com>
2024-09-04 17:44:35 -0300

    service employee2.0

---
+++ src/services/employee.ts
@@ -0,0 +1,41 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3006/api/employees'
+
+export interface IEmployee{
+   cuil: String,
+    dni: String,
+    name: String,
+    address: String,
+    email: String,
+    phone: String,
+}
+
+interface IEmployeeResponse{
+  data: IEmployee[]
+}
+
+interface IDeleteResponse{
+  message: string
+}
+
+const getAll = async (): Promise<IEmployee[]> =>{
+  const response = await axios.get<IEmployeeResponse>(baseUrl)
+  return response.data.data
+}
+
+const create = async (newObject: IEmployee): Promise<IEmployee> =>{
+  const response = await axios.post<{data: IEmployee}>(baseUrl, newObject)
+  return response.data.data
+}
+
+const update = async (id: string, newObject: IEmployee): Promise<IEmployee> =>{
+  const response = await axios.put<{data: IEmployee}>(`${baseUrl}/${id}`, newObject)
+  return response.data.data
+}
+
+const remove = async (id: string): Promise<IDeleteResponse> =>{
+  const response = await axios.delete<{data: IDeleteResponse}>(`${baseUrl}/${id}`)
+  return response.data.data
+}
+
+export default {getAll, create, update, remove}
