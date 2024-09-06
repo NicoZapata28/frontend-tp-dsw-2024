4d360ba7460430e879fe0e100605a99ca703c1ab
valentttino <vgboggio@gmail.com>
2024-09-05 21:32:33 -0300

    new design in OperationCards

--- src/components/OperationCards.tsx
+++ src/components/OperationCards.tsx
@@ -6,23 +6,23 @@ import Icon3 from '../img/customers-icon.svg'
 const OperationCards = () =>{
   return(
     <Container>
-      <Row className="mt-5 text-center">
+      <Row className="mt-5 text-center g-2">
         <Col md={4}>
-          <Button variant="outline-primary" className="icon-button" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-          <img src={Icon1} alt="Materials Icon" width="50" height="50" />
-            <div>Materials</div>
+          <Button variant="dark" className="icon-button mx-1" href="/Materials">
+          <img src={Icon1} alt="Materials Icon" width="90" height="90" style={{ filter: 'invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)' }}/>
+            <div style={{fontSize: '18px', marginTop:'10px'}}>Materials</div>
           </Button>
         </Col>
         <Col md={4}>
-          <Button variant="outline-primary" className="icon-button" onClick={() => window.scrollTo({ top: 500, behavior: 'smooth' })}>
-            <img src={Icon2} alt="Orders Icon" width="50" height="50" />
-            <div>Orders</div>
+          <Button variant="dark" className="icon-button mx-1" href="/Orders">
+            <img src={Icon2} alt="Orders Icon" width="90" height="90" style={{ filter: 'invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)' }}/>
+            <div style={{fontSize: '18px', marginTop:'10px'}}>Orders</div>
           </Button>
         </Col>
         <Col md={4}>
-          <Button variant="outline-primary" className="icon-button" onClick={() => window.scrollTo({ top: 1000, behavior: 'smooth' })}>
-            <img src={Icon3} alt="Customers Icon" width="50" height="50" />
-            <div>Customers</div>
+          <Button variant="dark" className="icon-button mx-1" href="/Customers">
+            <img src={Icon3} alt="Customers Icon" width="90" height="90" style={{ filter: 'invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)' }}/>
+            <div style={{fontSize: '18px', marginTop:'10px'}}>Customers</div>
           </Button>
         </Col>
       </Row>
