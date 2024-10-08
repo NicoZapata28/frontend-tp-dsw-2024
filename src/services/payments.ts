9f42942576bdec0cac64f667d597c428afab263a
NicoZapata28 <135772592+NicoZapata28@users.noreply.github.com>
2024-10-08 18:36:01 -0300

    details Orders and update payments service

--- src/services/payments.ts
+++ src/services/payments.ts
@@ -1,39 +1,47 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3006/api/payments'
 
-export interface IPayment{
-  paymentNumber: string,
-  orderNumber: string,
+export interface IInstallmentsDetails {
+  installmentN: number,
+  paymentDate: Date,
   amount: number,
-  orderDate: Date
+  paid: string // "Y" or "N"
 }
 
-interface IPaymentResponse{
+export interface IPayment {
+  idOrder: string,
+  numberOfInstallments: number,
+  paid: string, // "Y" or "N"
+  installmentsDetails: IInstallmentsDetails[],
+  id?: string // optional since the backend generates it
+}
+
+interface IPaymentResponse {
   data: IPayment[]
 }
 
-interface IDeleteResponse{
+interface IDeleteResponse {
   message: string
 }
 
-const getAll = async (): Promise<IPayment[]> =>{
+const getAll = async (): Promise<IPayment[]> => {
   const response = await axios.get<IPaymentResponse>(baseUrl)
   return response.data.data
 }
 
-const create = async (newObject: IPayment): Promise<IPayment> =>{
+const create = async (newObject: IPayment): Promise<IPayment> => {
   const response = await axios.post<IPayment>(baseUrl, newObject)
   return response.data
 }
 
-const update = async (paymentNumber: string, newObject: IPayment): Promise<IPayment> =>{
-  const response = await axios.put<IPayment>(`${baseUrl}/${paymentNumber}`, newObject)
+const update = async (id: string, newObject: IPayment): Promise<IPayment> => {
+  const response = await axios.put<IPayment>(`${baseUrl}/${id}`, newObject)
   return response.data
 }
 
-const remove = async (paymentNumber: string): Promise<IDeleteResponse> =>{
-  const response = await axios.delete<IDeleteResponse>(`${baseUrl}/${paymentNumber}`)
+const remove = async (id: string): Promise<IDeleteResponse> => {
+  const response = await axios.delete<IDeleteResponse>(`${baseUrl}/${id}`)
   return response.data
 }
 
-export default {getAll, create, update, remove}
+export default { getAll, create, update, remove }
