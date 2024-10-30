ae6442dca38076dd144c4991562079dddee93f17
NicoZapata28 <135772592+NicoZapata28@users.noreply.github.com>
2024-10-30 17:27:03 -0300

    update welcome and icons

--- src/components/OperationCards.tsx
+++ src/components/OperationCards.tsx
@@ -1,10 +1,10 @@
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import styles from '../styles/operationCards.module.css'
-import { Link } from 'react-router-dom'
-import Icon1 from '../img/materials-icon.svg'
-import Icon2 from '../img/orders-icon.svg'
-import Icon3 from '../img/customers-icon.svg'
-import Icon4 from '../img/balance-icon.svg'
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import styles from '../styles/operationCards.module.css';
+import { Link } from 'react-router-dom';
+import Icon1 from '../img/materials-icon.svg';
+import Icon2 from '../img/orders-icon.svg';
+import Icon3 from '../img/customers-icon.svg';
+import Icon4 from '../img/balance-icon.svg';
 
 const operations = [
   { path: '/Materials', icon: Icon1, label: 'Materials' },
@@ -16,14 +16,14 @@ const operations = [
 const OperationCards = () => {
   return (
     <Container>
-      <Row xs={1} md={2} className={styles.body}>
+      <Row xs={2} md={2} className={styles.body}>
         {operations.map((operation, idx) => (
           <Col key={idx} className="d-flex justify-content-center">
             <Link to={operation.path} style={{ textDecoration: 'none' }}>
               <Card className={`${styles.card} text-center`} style={{ cursor: 'pointer' }}>
-                <Card.Img variant="top" src={operation.icon} style={{ width: '90px', height: '90px', margin: 'auto' }} />
+                <Card.Img variant="top" className={styles.icon} src={operation.icon} alt={`Icono de ${operation.label}`} style={{ width: '90px', height: '90px' }} />
                 <Card.Body>
-                  <Card.Title>{operation.label}</Card.Title>
+                  <Card.Title className={styles.cardTitle}>{operation.label}</Card.Title>
                 </Card.Body>
               </Card>
             </Link>
