b176843f08256695e0a825e07f831d01ffa13dd8
valentttino <99833062+valentttino@users.noreply.github.com>
2024-11-06 19:18:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,49 @@
-import { useState, useEffect } from "react";
-import Navigation from "./components/Navigation";
-import Materials from "./components/Materials";
-import Orders from "./components/Orders";
-import Home from "./components/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from "./components/Footer";
-import Customers from "./components/Customers";
-import LoginForm from "./components/LoginForm";
-import { jwtDecode } from 'jwt-decode';
+import { useState, useEffect } from "react"
+import Navigation from "./components/Navigation"
+import Materials from "./components/Materials"
+import Orders from "./components/Orders"
+import Home from "./components/Home"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from "./components/Footer"
+import Customers from "./components/Customers"
+import LoginForm from "./components/LoginForm"
+import { jwtDecode } from 'jwt-decode'
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [employeeName, setEmployeeName] = useState<string>('');
-  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+  const [employeeName, setEmployeeName] = useState<string>('')
 
   interface DecodedToken {
-    id: string;
-    cuil: string;
-    name: string;
-    exp: number;
-    iat: number;
+    id: string
+    cuil: string
+    name: string
+    exp: number
+    iat: number
   }
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    console.log("Token:", token);
+    const token = localStorage.getItem('token')
+    console.log("Token:", token)
     if (token) {
-      const decodedToken = jwtDecode<DecodedToken>(token);
-      setEmployeeName(decodedToken.name);
-      console.log(decodedToken);
-      setIsLoggedIn(true);
-    }
-
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+      const decodedToken = jwtDecode<DecodedToken>(token)
+      setEmployeeName(decodedToken.name)
+      console.log(decodedToken)
+      setIsLoggedIn(true)
+    } }, [])
 
   if (!isLoggedIn) {
-    return <LoginForm onLoginSuccess={() => setIsLoggedIn(true)} />;
+    return <LoginForm onLoginSuccess={() => setIsLoggedIn(true)} />
   }
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
+    localStorage.removeItem('token')
+    setIsLoggedIn(false)
   };
 
   return (
     <div className="d-flex flex-column min-vh-100">
       <Router>
-        <Navigation onLogout={handleLogout} employeeName={employeeName} isMobile={isMobile} />
+        <Navigation onLogout={handleLogout} employeeName={employeeName}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/materials" element={<Materials />} />
@@ -62,7 +53,7 @@ const App = () => {
         <Footer />
       </Router>
     </div>
-  );
-};
+  )
+}
 
 export default App;
