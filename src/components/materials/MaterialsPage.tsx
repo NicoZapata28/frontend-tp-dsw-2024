d528a9c9b9f7a4ca2cf7f02754a0659f1aae7f39
valentttino <vgboggio@gmail.com>
2024-11-07 14:51:18 -0300

    add materialsService

--- src/components/materials/MaterialsPage.tsx
+++ src/components/materials/MaterialsPage.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { IMaterial } from '../../services/materials.ts'
+import materialsService from '../../services/materials.ts'
 import './MaterialsPage.css'
 
 const MaterialsPage: React.FC = () =>{
+  const [materials, setMaterials] = useState<IMaterial[]>([])
+
+  useEffect(() => {
+    materialsService.getAll().then((data) => {
+      setMaterials(data)
+    })
+  }, [])
+
+  console.log(materials)
   return(
     <div className='materials-page'>
 
